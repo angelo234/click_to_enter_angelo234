f3f8e39cacd752cacb83b9dfbebe52e8be6db384
Angelo Matteo <angmatt90@gmail.com>
2024-09-07 16:46:05 -0700

    User now can hold middle click down and move cursor over desired vehicle to enter, as vehicle bounding box isn't always so obvious, so you don't need to middle click spam to attempt to enter it (e.g. for the cone, the hitbox seems to be offset half a meter from the center).

--- scripts/click_to_enter_angelo234/extension.lua
+++ scripts/click_to_enter_angelo234/extension.lua
@@ -1,39 +1,54 @@
+-- This Source Code Form is subject to the terms of the bCDDL, v. 1.1.
+-- If a copy of the bCDDL was not distributed with this
+-- file, You can obtain one at http://beamng.com/bCDDL-1.1.txt
+
 local M = {}
 
-local function getPlayerUnicycle()
-  local veh = be:getPlayerVehicle(0)
-  if veh and veh:getJBeamFilename() == "unicycle" then
-    return veh
-  end
-end
+local raycastFlags = bit.bor(SOTVehicle)
+
+local selectingVehicle = false
 
 local function setUnicycleInactive(unicycle)
   be.nodeGrabber:clearVehicleFixedNodes(unicycle:getID())
   unicycle:setActive(0)
 end
 
-local function onVehicleSelected()
-	local flags = bit.bor(SOTVehicle)
-	
-	local raycast_info = cameraMouseRayCast(true, flags)
-	
-	if raycast_info then
-		local veh = be:getPlayerVehicle(0)
-		
-		if veh:getID() ~= raycast_info.object:getID() then
-		
-			local unicycle = getPlayerUnicycle()
-			be:enterVehicle(0, raycast_info.object)
-			if unicycle then
-				setUnicycleInactive(unicycle)
-			end	
-			
-			core_camera.setByName(0, "orbit", true)
-			commands.setGameCamera()
-		end
-	end
+local function enterVehicle(veh)
+  be:enterVehicle(0, veh)
+  selectingVehicle = false
 end
 
-M.onVehicleSelected = onVehicleSelected
+-- While the user is holding the middle click down, every frame, send a raycast out until a vehicle is hit
+-- Once a vehicle is hit, enter the vehicle and stop raycasting even if middle click is down to prevent entering another vehicle accidentally
+-- Only once middle click is lifted will the user be able to begin another raycasting
+local function onUpdate(dt)
+  if not selectingVehicle then return end
+
+  local raycastInfo = cameraMouseRayCast(true, raycastFlags)
+  if not raycastInfo then return end
+
+  local veh = be:getPlayerVehicle(0)
+  if veh then
+    -- Don't allow entering vehicle currently in
+    if veh:getID() ~= raycastInfo.object:getID() then
+      enterVehicle(raycastInfo.object)
+      if veh:getJBeamFilename() == "unicycle" then
+        setUnicycleInactive(veh)
+      end
+      core_camera.setByName(0, "orbit", true)
+      commands.setGameCamera()
+    end
+  else
+    enterVehicle(raycastInfo.object)
+  end
+end
+
+-- Called by input actions on input changed
+local function selectVehicle(controllerState)
+  selectingVehicle = controllerState == 1
+end
+
+M.onUpdate = onUpdate
+M.selectVehicle = selectVehicle
 
 return M
